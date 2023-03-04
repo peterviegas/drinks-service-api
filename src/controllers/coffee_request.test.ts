b9c8d48eaf7dd3679fbe1215b68acfe48311920e
Peter Viegas <peterviegas@outlook.com>
2023-03-04 10:01:39 +0000

    feat: Implement GET request for coffee_request used res.json

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,11 +1,13 @@
 import request from 'supertest';
 import { app } from '../app';
 
-// ⚠️ This should error as "../app" doesn't exist yet!
-describe('Test home API endpoint request', () => {
-	test('GET should return correct message', async () => {
-		const res = await request(app).get('/');
+test('GET /coffee should return correct object', async () => {
+	const res = await request(app)
+		.get('/coffee')
+		.query({ coffeeName: 'Latte' });
 		expect(res.statusCode).toEqual(200);
-		expect(res.text).toEqual('Welcome to the Drinks API!');
+		expect(res.body).toEqual({
+		drinkType: 'Coffee',
+		name: 'Latte',
 	});
 });
