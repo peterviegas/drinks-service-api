fd30ae28aa0316850f14255633e58a9816b6199d
Peter Viegas <peterviegas@outlook.com>
2023-03-04 18:50:55 +0000

    Added more tests to coffee request.test.ts

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -11,3 +11,31 @@ test('GET /coffee should return correct object', async () => {
 		name: 'Latte',
 	});
 });
+
+describe('Test coffee API endpoint request', () => {
+	test('GET /coffeelover should return correct message', async() => {
+		const res = await request(app).get('/coffeelover');
+		expect(res.statusCode).toEqual(200);
+		expect(res.text).toEqual('I like coffee!');
+	});
+});
+
+test('GET /coffee with no param should return correct object', async () => {
+	const res = await request(app)
+		.get('/coffee')
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toEqual({
+		drinkType: 'Coffee',
+		name: 'Latte',
+	});
+});
+test('GET /coffee with different param should return correctobject', async () => {
+	const res = await request(app)
+		.get('/coffee')
+		.query({ coffeeName: 'Macchiato' });
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toEqual({
+		drinkType: 'Coffee',
+		name: 'Macchiato',
+	});
+});
